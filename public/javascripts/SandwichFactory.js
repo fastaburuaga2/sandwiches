901eee6e734943258b6e3a8cabefb9b9db3962e8
jpkramer <jpkcoding@gmail.com>
2015-12-13 22:22:04 +0800

    minor

--- public/javascripts/SandwichFactory.js
+++ public/javascripts/SandwichFactory.js
@@ -1,6 +1,43 @@
 angular.module('sandwiches').factory('SandwichFactory', [function(){
-  var o = {
-    sandwiches: []
+  var api = {};
+
+  api.sandwiches = [];
+
+  api.login = function (username, password) {
+        // send along page information and the category type
+        return $http.post("/login", {userid: username, passwd: password);
   };
-  return o;
+
+  api.logout = function (username, password) {
+        // send along page information and the category type
+        return $http.get("/logout", {username: username, password: password);
+  };
+
+  api.me = function (username, password) {
+        // send along page information and the category type
+        return $http.post("/login", {username: username, password: password);
+  };
+
+  api.getSandwiches = function (username, password) {
+        // send along page information and the category type
+        return $http.post("/login", {username: username, password: password);
+  };
+
+  api.createSandwich = function (username, password) {
+        // send along page information and the category type
+        return $http.post("/login", {username: username, password: password);
+  };
+
+  api.editSandwich = function (username, password) {
+        // send along page information and the category type
+        return $http.post("/login", {username: username, password: password);
+  };
+
+  api.deleteSandwich = function (username, password) {
+        // send along page information and the category type
+        return $http.post("/login", {username: username, password: password);
+  };
+
+
+  return api;
 }]);
