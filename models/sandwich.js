d7b90204c7bd98b9b59d1d0c6a937e94df30c294
EnriqueM <enrique.marr@gmail.com>
2015-12-13 22:46:06 +0800

    order, user, sandwich models

--- models/sandwich.js
+++ models/sandwich.js
@@ -2,48 +2,47 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Sandwich = new Schema({
-    userid: ObjectId,
-    orderCount: Number,
+    orderCount: { type: Number, default: 0 },
     breadtype: {
-      wheat: Boolean,
-      oat: Boolean,
-      italian: Boolean,
-      parmesano: Boolean
+      wheat: { type: Boolean, required: true },
+      oat: { type: Boolean, required: true },
+      italian: { type: Boolean, required: true },
+      parmesano: { type: Boolean, required: true }
     },
-    halfSize: Boolean,
+    halfSize: {type: Boolean, required: true},
     meats: {
-      chicken: Boolean,
-      italian: Boolean,
-      turkey: Boolean,
-      tuna: Boolean,
-      meatball: Boolean,
-      ham: Boolean
+      chicken: { type: Boolean, required: true },
+      italian: { type: Boolean, required: true },
+      turkey: { type: Boolean, required: true },
+      tuna: { type: Boolean, required: true },
+      meatball: { type: Boolean, required: true },
+      ham: { type: Boolean, required: true }
     },
     vegetables: {
-      lettuce: Boolean,
-      tomatoes: Boolean,
-      cucumbers: Boolean,
-      pickles: Boolean,
-      peppers: Boolean,
-      olives: Boolean,
-      onions: Boolean,
-      jalapenos: Boolean
+      lettuce: { type: Boolean, required: true },
+      tomatoes: { type: Boolean, required: true },
+      cucumbers: { type: Boolean, required: true },
+      pickles: { type: Boolean, required: true },
+      peppers: { type: Boolean, required: true },
+      olives: { type: Boolean, required: true },
+      onions: { type: Boolean, required: true },
+      jalapenos: { type: Boolean, required: true }
     },
     cheese: {
-      swiss: Boolean,
-      cheddar: Boolean,
-      american: Boolean,
-      mozzarella: Boolean
+      swiss: { type: Boolean, required: true },
+      cheddar: { type: Boolean, required: true },
+      american: { type: Boolean, required: true },
+      mozzarella: { type: Boolean, required: true }
     },
     sauce: {
-      mustard: Boolean,
-      onion: Boolean,
-      mayo: Boolean,
-      oliveoil: Boolean,
-      chilli: Boolean,
-      ketchup: Boolean,
-      bbq: Boolean,
-      ranch: Boolean
+      mustard: { type: Boolean, required: true },
+      onion: { type: Boolean, required: true },
+      mayo: { type: Boolean, required: true },
+      oliveoil: { type: Boolean, required: true },
+      chilli: { type: Boolean, required: true },
+      ketchup: { type: Boolean, required: true },
+      bbq: { type: Boolean, required: true },
+      ranch: { type: Boolean, required: true }
     },
     instruction: String
 });
--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-  sandwiches: [ObjectId]
+  sandwiches: [ObjectId],
+  sanwichescount: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('user', User);
