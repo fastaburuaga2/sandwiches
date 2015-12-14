40b0930bca7aba1dc0d8304c40e87b41254325fc
fastaburuaga22 <fastaburuaga@uc.cl>
2015-12-14 18:59:12 +0800

    heroku settings

--- server.js
+++ server.js
@@ -28,10 +28,11 @@ mongoose.connection.on('open', function() {
 
 app.use('/', sandwichAPIRoutes);
 app.use('/', authAPIRoutes);
-
+/*
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Sandwich app listening at http://%s:%s', host, port);
 });
+*/
